c46e612379ea2143adc4e80b285270b69526e5e5
Dan F-M <danfm@nyu.edu>
2016-10-05 14:45:27 -0700

    step

--- paper/figures/abctoy.py
+++ paper/figures/abctoy.py
@@ -19,7 +19,7 @@ np.random.seed(42)
 def simulator(ln_mu, size=None):
     return np.random.poisson(np.exp(ln_mu), size=size)
 
-true_rate = 6.89
+true_rate = 6.78
 prior_range = np.array([-5.0, 3.0])
 # true_rate = 50.345
 # prior_range = np.array([0, 4.5])
@@ -55,17 +55,20 @@ def pseudo_log_probability_function(N_obs, S, eps, ln_mu):
     dist = N_sim - N_obs
     return lnprior(ln_mu) + np.logaddexp.reduce(-0.5 * (dist / eps)**2)
 
-pseudo_log_probability = partial(pseudo_log_probability_function, N_obs, 50,
-                                 1e-3)
+S = 50
+eps_pow = -3
+pseudo_log_probability = partial(pseudo_log_probability_function, N_obs, S,
+                                 10**eps_pow)
 pseudo_chain, _ = mh(pseudo_log_probability, np.log([N_obs]), 500000)
 
 fig, ax = plt.subplots(1, 1, figsize=SQUARE_FIGSIZE)
 
 bins = np.linspace(0, np.exp(prior_range[1]), 50)
-ax.hist(np.exp(heuristic_chain), bins, normed=True, histtype="step", lw=1.5,
-        label="heuristic")
-ax.hist(np.exp(pseudo_chain), bins, normed=True, histtype="step", lw=1.5,
-        label="abc")
+# x = 0.5*(bins[1:] + bins[:-1])
+y, _ = np.histogram(np.exp(heuristic_chain), bins, density=True)
+ax.step(bins, np.append(0, y), lw=1.5, label="heuristic")
+y, _ = np.histogram(np.exp(pseudo_chain), bins, density=True)
+ax.step(bins, np.append(0, y), lw=1.5, label="abc")
 ax.plot(mu_grid, true_posterior, color="k", lw=1.5, label="exact", alpha=0.8)
 ax.axvline(true_rate, lw=3, color="k", alpha=0.3)
 
@@ -76,9 +79,26 @@ ax.set_xlim(0, np.exp(prior_range[1]))
 ax.set_ylim(0, 1.1 * true_posterior.max())
 ax.legend(fontsize=12)
 
-q = np.percentile(np.exp(heuristic_chain), [16, 50, 84])
-print(true_rate, q[1], np.diff(q))
-q = np.percentile(np.exp(pseudo_chain), [16, 50, 84])
-print(true_rate, q[1], np.diff(q))
+with open("abctoy.tex", "w") as f:
+    f.write("\\newcommand{{\\abctoytruth}}{{{{\ensuremath{{{0:.2f}}}}}}}\n"
+            .format(true_rate))
+    f.write("\\newcommand{{\\abctoynobs}}{{{{\ensuremath{{{0:.0f}}}}}}}\n"
+            .format(N_obs))
+    f.write("\\newcommand{{\\abctoyeps}}{{{{\ensuremath{{10^{{{0:d}}}}}}}}}\n"
+            .format(eps_pow))
+    f.write("\\newcommand{{\\abctoyS}}{{{{\ensuremath{{{0:d}}}}}}}\n"
+            .format(S))
+
+    q = np.percentile(np.exp(heuristic_chain), [16, 50, 84])
+    f.write("\\newcommand{\\abctoyheuristic}{{\ensuremath{")
+    f.write("{0:.2f}^{{+{1:.2f}}}_{{{2:.2f}}}"
+            .format(q[1], q[2]-q[1], q[1]-q[0]))
+    f.write("}}}\n")
+
+    q = np.percentile(np.exp(pseudo_chain), [16, 50, 84])
+    f.write("\\newcommand{\\abctoyabc}{{\ensuremath{")
+    f.write("{0:.2f}^{{+{1:.2f}}}_{{{2:.2f}}}"
+            .format(q[1], q[2]-q[1], q[1]-q[0]))
+    f.write("}}}\n")
 
 savefig(fig, "abctoy.pdf")
